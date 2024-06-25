74933f84d9e06756c6e24f68939169f5e01528de
QiaoJiazhou <qiaojiazhou@outlook.com>
2024-06-25 18:00:22 +0800

    feat: init

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,8 +6,8 @@ export default defineUserConfig({
   base: "/benborba/",
 
   lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  title: "奔波霸",
+  description: "奔波霸的博客",
 
   theme,
 
